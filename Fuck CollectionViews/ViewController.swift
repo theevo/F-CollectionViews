01d3bc60a48c1fed3c1c7bd8f655cc290ff8f893
Theo Vora <theo.vora@gmail.com>
2022-09-15 17:18:08 -0400

    show 3 red cells

--- Fuck CollectionViews/ViewController.swift
+++ Fuck CollectionViews/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var pizzaCollectionView: UICollectionView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pizzaCollectionView.delegate = self
+        pizzaCollectionView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pizzaCell", for: indexPath)
+        cell.backgroundColor = UIColor.systemRed
+        return cell
+    }
+    
+    
+}
