756e45e1e7c6d7c7a1162d7ed84bb71cf107101d
Theo Vora <theo.vora@gmail.com>
2022-09-15 17:00:41 -0400

    Initial Commit

---
+++ Fuck CollectionViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fuck CollectionViews
+//
+//  Created by Theo Vora on 9/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
