b2b8deba44a9fd2279403c6d1bfb7e2e59f7790c
Theo Vora <theo.vora@gmail.com>
2022-09-16 15:17:57 -0400

    paginate the cells

--- Fuck CollectionViews/ViewController.swift
+++ Fuck CollectionViews/ViewController.swift
@@ -9,20 +9,28 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate {
     
+    let pizzas = Array(repeating: UIImage(named: "PepCheese"), count: 10)
+    
     // MARK: - Outlets
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var pizzaCollectionView: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var label: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        pizzaCollectionView.delegate = self
-        pizzaCollectionView.dataSource = self
         
+        initSetup()
         applyConstraints()
         applyColors()
+    }
+    
+    func initSetup() {
+        pizzaCollectionView.delegate = self
+        pizzaCollectionView.dataSource = self
+        pizzaCollectionView.isPagingEnabled = true
+        pizzaCollectionView.showsHorizontalScrollIndicator = false
+        pageControl.numberOfPages = pizzas.count
     }
     
     func applyConstraints() {
@@ -37,20 +45,28 @@ class ViewController: UIViewController, UICollectionViewDelegate {
     func applyColors() {
         stackView.border(color: .systemGray, thickness: 0.4)
         pizzaCollectionView.border(color: .blue)
-        pageControl.border(color: .red)
+//        pageControl.border(color: .red)
         label.border(color: .green)
+        pageControl.currentPageIndicatorTintColor = .systemGray
+        pageControl.pageIndicatorTintColor = .systemGray3
+        pageControl.backgroundColor = .systemGray6
+        pageControl.layer.cornerRadius = 12.0
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        pageControl.currentPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
     }
 }
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        pizzas.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pizzaCell", for: indexPath) as? PizzaCollectionViewCell else { return UICollectionViewCell() }
-        cell.backgroundColor = UIColor.systemRed
-        cell.image = UIImage(named: "PepCheese")
+        cell.border(color: .red)
+        cell.image = pizzas[indexPath.item]
         return cell
     }
     
